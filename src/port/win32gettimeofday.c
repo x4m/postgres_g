dd3175000f89cad8f9227a0589592a151f374a3a
Andrey M. Borodin <x4mmm@night.local>
2024-03-20 22:30:14 +0500

    Implement UUID v7
    This commit adds function for UUID generation. Most important function here
    is uuidv7() which generates new UUID according to the new standard.
    For code readability this commit adds alias uuidv4() to function
    gen_random_uuid().
    
    Author: Andrey Borodin
    Reviewed-by: Sergey Prokhorenko, Kirk Wolak, Przemysław Sztoch
    Reviewed-by: Nikolay Samokhvalov, Jelte Fennema-Nio, Aleksander Alekseev
    Reviewed-by: Peter Eisentraut, Chris Travers, Lukas Fittl
    Reviewed-by: Michael Paquier, Masahiko Sawada, Stepan Neretin
    Discussion: https://postgr.es/m/CAAhFRxitJv%3DyoGnXUgeLB_O%2BM7J2BJAmb5jqAT9gZ3bij3uLDA%40mail.gmail.com

--- src/port/win32gettimeofday.c
+++ src/port/win32gettimeofday.c
@@ -42,7 +42,6 @@ static const unsigned __int64 epoch = UINT64CONST(116444736000000000);
 #define FILETIME_UNITS_PER_SEC	10000000L
 #define FILETIME_UNITS_PER_USEC 10
 
-
 /*
  * timezone information is stored outside the kernel so tzp isn't used anymore.
  *
