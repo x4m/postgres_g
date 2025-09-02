4355ac06d8d0fdb07bbbb580adba412045cd791f
Melanie Plageman <melanieplageman@gmail.com>
2025-09-02 15:22:11 -0400

    Add database Oid to CkptSortItem
    This is an oversight that currently isn't causing harm. However, it is
    required for checkpointer write combining -- which will be added in
    a future commit.
    
    Author: Melanie Plageman <melanieplageman@gmail.com>
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://postgr.es/m/2FA0BAC7-5413-4ABD-94CA-4398FE77750D%40gmail.com

--- src/include/storage/buf_internals.h
+++ src/include/storage/buf_internals.h
@@ -516,6 +516,7 @@ extern uint64 WaitBufHdrUnlocked(BufferDesc *buf);
 typedef struct CkptSortItem
 {
 	Oid			tsId;
+	Oid			dbId;
 	RelFileNumber relNumber;
 	ForkNumber	forkNum;
 	BlockNumber blockNum;
