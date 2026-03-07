1a70f773287bc6848a3b6d87984f844d4517355c
Evgeny Voropaev <evorop@gmail.com>
2026-03-07 18:05:39 +0800

    Implement vect and uniqsortvect containers and bitpack algorithms.
    The vect container stores arrays of integers and provides a set of
    algorithms implementing essential operations on the contained array,
    such as initialization, appending, inserting, and clearing.
    
    The uniqsortvect container is based on the vect type but assumes that
    its elements are sorted and unique. In addition to the algorithms
    provided by vect, uniqsortvect implements binary search and the
    specialized insertion routine.
    
    The containers support both external memory provided by a caller and
    automatically managed memory using malloc, Postgres's palloc, or similar
    allocation functions. A container's strategy regarding memory management
    must be set at container initialization, and all subsequent operations
    honor this configuration. For example, a caller can place a buffer on
    the stack to avoid heap allocation and pass the buffer to a vector
    instance, which results in the vector performs no dynamic allocation.
    
    This commit also introduces the bitpack unit, which provides algorithms
    for dense bit-level packing and unpacking. The bitpack unit does not
    use dynamic memory.
    
    Each unit (vect, bitpack) is implemented as a set of templates that
    allow developers to generate specialized solutions for any integer type
    (uint8, int8, uint16, int16, and so on). The units bitpack_u16 and
    vect_u16 supporting the uint16_t type are also provided by this commit.
    
    Unit tests for the provided implementations are included. Unit tests are
    implemented as binary applications written in C language
    (ELF executables) that support the TAP protocol and are run using the
    Prove utility.
    
    The new Makefile target, check-unit, is integrated into the PostgreSQL
    build system and allows running the unit tests using the command 'make
    check-unit'.
    
    Author: Evgeny Voropaev <evgeny.voropaev@tantorlabs.com> <evorop@gmail.com>

---
+++ src/include/lib/vect_u16.h
@@ -0,0 +1,34 @@
+/*
+ * File: vect_u16.h
+ */
+
+#ifndef _VECT_U16_H_
+#define _VECT_U16_H_
+
+/* clang-format off */
+#include "vect_u16_config.h"
+#include "vect_templ.h"
+/* clang-format on */
+
+/*
+ * Types are supposed to be created created by this file
+ *     vect_u16_t
+ *     item_u16_t
+ *     uniqsortvect_u16_t
+ */
+/*
+ * Functions are supposed to be created by this file
+ *     vect_u16_create
+ *     vect_u16_create_filled
+ *     vect_u16_reserve
+ *     vect_u16_append
+ *     vect_u16_destroy
+ *     vect_u16_print
+ *     vect_u16_compare
+ *     vect_u16_insert
+ *     vect_u16_clear
+ *     usv_u16_insert
+ *     usv_u16_search
+ */
+
+#endif //_VECT_U16_H_
